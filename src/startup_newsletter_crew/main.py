a1ba1788262dced24d0b825bbf7cc9d9d58d0384
andreasklumpp <andreas@klumpp-besigheim.de>
2025-09-17 15:40:29 +0200

    Create initial research crew

--- src/startup_newsletter_crew/main.py
+++ src/startup_newsletter_crew/main.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
-import sys
 import warnings
-
 from datetime import datetime
+from dotenv import load_dotenv
 
 from startup_newsletter_crew.crew import StartupNewsletterCrew
 
-warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+load_dotenv(override=True)
 
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
 def run():
     """
@@ -23,46 +19,9 @@ def run():
     }
     
     try:
-        StartupNewsletterCrew().crew().kickoff(inputs=inputs)
+        result = StartupNewsletterCrew().crew().kickoff(inputs=inputs)
+        print(result.raw)
     except Exception as e:
         raise Exception(f"An error occurred while running the crew: {e}")
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
-    }
-    try:
-        StartupNewsletterCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        StartupNewsletterCrew().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    
-    try:
-        StartupNewsletterCrew().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
